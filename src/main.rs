5631377c4d500b6da2aa0354281c196715eab173
Uwe Pachler <uwe.pachler@gmail.com>
2024-01-30 19:49:21 +0100

    app now has two states, using the same main window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 slint::include_modules!();
+
+
 fn main() {
-    WelcomeWindow::new().unwrap().run().unwrap();
+    let main_window = MainWindow::new().unwrap();
+    let main_window_weak = main_window.as_weak();
+    main_window.global::<AppUI>().on_login_triggered(move ||{
+        // MISSING: actual login via OIDC & Browser
+        main_window_weak.unwrap().global::<AppUI>().set_state(MainWindowState::ShowingSitePresenceView);
+    });
+    main_window.run().unwrap();
+
 }
